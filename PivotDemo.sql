96ca90b94d1fff7d533b4345c19aa0b6f87e712a
vaddebalu <vadde.balu@gmail.com>
2023-11-16 18:18:40 +0530

    Usage of Pivot in BigQuery

---
+++ PivotDemo.sql
@@ -0,0 +1,16 @@
+--Using Pivot In BigQUery
+
+create table employee.produce AS (
+  SELECT 'Kale' as product, 51 as sales, 'Q1' as quarter, 2020 as year UNION ALL
+  SELECT 'Kale', 23, 'Q2', 2020 UNION ALL
+  SELECT 'Kale', 45, 'Q3', 2020 UNION ALL
+  SELECT 'Kale', 3, 'Q4', 2020 UNION ALL
+  SELECT 'Kale', 70, 'Q1', 2021 UNION ALL
+  SELECT 'Kale', 85, 'Q2', 2021 UNION ALL
+  SELECT 'Apple', 77, 'Q1', 2020 UNION ALL
+  SELECT 'Apple', 0, 'Q2', 2020 UNION ALL
+  SELECT 'Apple', 1, 'Q1', 2021)
+
+select * from `employee.produce` limit 10
+
+select * from `employee.produce` pivot (sum(sales) for quarter in ('Q1','Q2','Q3','Q3'))
